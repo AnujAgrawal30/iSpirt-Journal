3dd9c9c2d8ad55bd9bb3c8b866d760828459fb6a
vipin14444 <vipin.sharma3039@gmail.com>
2021-02-01 21:36:13 +0530

    UI Changes

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,5 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ApiService } from '../api.service';
 import { Article } from '../models';
@@ -18,6 +18,7 @@ import { Router } from '@angular/router';
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
+  encapsulation: ViewEncapsulation.None
   // providers: [
   //   {
   //     provide: APP_BASE_HREF,
@@ -32,7 +33,7 @@ export class ArticleComponent implements OnInit {
   article: Article | undefined;
   content: any;
 
-  go_home(){
+  go_home() {
     this.router.navigate(['/'])
   }
 
@@ -41,52 +42,52 @@ export class ArticleComponent implements OnInit {
     console.log(article_index)
     console.log(window.location.href);
     this.api.get_article(article_index)
-    .subscribe (
-      response => {
-        this.article = response;
-        console.log(this.article);
-        var newStr = this.article.content_html.replace(/(<a href=")?((https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)))(">(.*)<\/a>)?/gi, function () {
-    return '<a target="blank" style="color:unset" href="' + arguments[2] + '">' + (arguments[7] || arguments[2]) + '</a>'
-});
-        this.content = this.sanitized.bypassSecurityTrustHtml(newStr);
+      .subscribe(
+        response => {
+          this.article = response;
+          console.log(this.article);
+          var newStr = this.article.content_html.replace(/(<a href=")?((https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)))(">(.*)<\/a>)?/gi, function () {
+            return '<a target="blank" style="color:unset" href="' + arguments[2] + '">' + (arguments[7] || arguments[2]) + '</a>'
+          });
+          this.content = this.sanitized.bypassSecurityTrustHtml(newStr);
 
 
 
-        // this.article.content_html
-        // var link = window.localStorage.href;
-        if (window.location.href.indexOf('#') != -1){
-          window.location.href = window.location.href.split('#')[0];
-        }
-        setTimeout(() => {
-          var elements: HTMLCollectionOf<HTMLAnchorElement> = document.getElementsByTagName('a');
-          console.log(elements.length);
-          for (let i = 0; i < elements.length; i++){
-            if (elements[i].href.indexOf('#') != -1){
-              elements[i].href = window.location.href + "#" + elements[i].href.split('#').pop();
-            }
-            
+          // this.article.content_html
+          // var link = window.localStorage.href;
+          if (window.location.href.indexOf('#') != -1) {
+            window.location.href = window.location.href.split('#')[0];
           }
-        }, 1000);
+          setTimeout(() => {
+            var elements: HTMLCollectionOf<HTMLAnchorElement> = document.getElementsByTagName('a');
+            console.log(elements.length);
+            for (let i = 0; i < elements.length; i++) {
+              if (elements[i].href.indexOf('#') != -1) {
+                elements[i].href = window.location.href + "#" + elements[i].href.split('#').pop();
+              }
+
+            }
+          }, 1000);
 
 
 
-      // $("a[href^='\#']").each(function(){
-      //   (this as HTMLLinkElement).href=location.href.split("#")[0]+'#'+(this as HTMLLinkElement).href.substr((this as HTMLLinkElement).href.indexOf('#')+1);
-      //   console.log(this.innerHTML);
-      // });
-        // var indices: number[] = [];
-        // var i = 0;
-        // while (this.article.content_html.indexOf('#', i) != -1){
-        //   i = this.article.content_html.indexOf('#');
-        //   indices.push(i);
-        // }
-        // var id = this.article.id;
-        // for (let i of indices) {
-        //   this.article.content_html = this.article.content_html.substr(0, i) + '/articles/' + id + this.article.content_html.substr(i);
-        // }
-      },
-      error => {console.log(error);}
-    )
+          // $("a[href^='\#']").each(function(){
+          //   (this as HTMLLinkElement).href=location.href.split("#")[0]+'#'+(this as HTMLLinkElement).href.substr((this as HTMLLinkElement).href.indexOf('#')+1);
+          //   console.log(this.innerHTML);
+          // });
+          // var indices: number[] = [];
+          // var i = 0;
+          // while (this.article.content_html.indexOf('#', i) != -1){
+          //   i = this.article.content_html.indexOf('#');
+          //   indices.push(i);
+          // }
+          // var id = this.article.id;
+          // for (let i of indices) {
+          //   this.article.content_html = this.article.content_html.substr(0, i) + '/articles/' + id + this.article.content_html.substr(i);
+          // }
+        },
+        error => { console.log(error); }
+      )
 
     // $().ready(function() {
     // });
